f611a9f77be4d39aa6bc65b11887c853d7ffbcf4
Worawiboon <57031266+Worawiboon@users.noreply.github.com>
2020-03-03 02:53:41 +0700

    update 1

---
+++ app/src/main/java/th/ac/su/monsterfarm/data/Monster.kt
@@ -0,0 +1,11 @@
+package th.ac.su.monsterfarm.data
+
+data class Monster (
+    val imageFile: String,
+    val monsterName: String,
+    val caption: String,
+    val description: String,
+    val price: Double,
+    val scariness: Int
+
+)
